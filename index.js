40940a1e6bcc14eaa011902f88e0798d58ca0895
Dagim Alemu <143899556+dagialemu@users.noreply.github.com>
2023-11-16 15:32:22 +0300

    classlist

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const form = document.querySelector(".quiz-form");
-const correctAnswer = ["A","A","A","A","A"]
-form.addEventListener("submit", ()=>{
-    event.preventDefault();
+const form = document.querySelector('.quiz-form');
+const correctAnswers = ['A','A','A','A','A']
+const result = document.querySelector('.result');
+const questions = document.querySelectorAll('.question');
+form.addEventListener('submit', event =>{
 
-    let score = 0;
-    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value]a
-  console.log(userAnswer)
+  event.preventDefault();
+  
+  const userAnswer = [form.q1.value, form.q2.value,form.q3.value, form.q4.value, form.q5.value]
+  let score = 0;
+  for(let i = 0; i< userAnswer.length; i++){
 
+    if(userAnswer[i] === correctAnswers[i]){
+      score ++
+      questions[i].classList.add('correct');
 
+    } else{
+      questions[i].classList.add('wrong');
+    }
+  }
+  scrollTo(0,0)
+  result.classList.remove('hide')
+  result.querySelector('p').textContent = `You scored ${score}/5`
 })
+
