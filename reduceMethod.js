bc49f2387854e233f74ff5c0abf1e8791fc746ce
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-11-21 14:39:14 +0000

    student grade sum with reduce

--- reduceMethod.js
+++ reduceMethod.js
@@ -1,9 +1,28 @@
-const numbers = [10,20,30,40,50];
+// const numbers = [10,20,30,40,50];
 
-const reduceNum = numbers.reduce((total, current) =>{
-  total+= current;
+// const sum = numbers.reduce((total, current)=>{
+
+//    total += current;
+//    return total;
+// }, 1) // the code will start with a total 1.
+
+// console.log(sum)
+
+const students = [
+  { name: 'Quincy', grade: 96 },
+  { name: 'Jason', grade: 84 },
+  { name: 'Alexis', grade: 100 },
+  { name: 'Sam', grade: 65 },
+  { name: 'Ayele', grade: 97},
+  { name: 'Peter', grade: 75 },
+  { name: 'Gabe', grade: 90 },
+  { name: 'Josh', grade: 99 },
+  { name: 'Maria', grade: 70 },
+  { name: 'Hannah', grade: 85 }
+];
+
+const sum = students.reduce((total, current) => {
+  total += current.grade;
   return total;
-  
 }, 0)
-
-console.log(reduceNum);
+console.log(sum)
