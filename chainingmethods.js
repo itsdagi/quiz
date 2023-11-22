8e1fa0694751d5e3dabf2aaa10fd874167d88b2b
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-11-22 09:03:20 +0000

    shorter way to impliment it

--- chainingmethods.js
+++ chainingmethods.js
@@ -1,7 +1,5 @@
 const numbers =[1,5,6,4,3,2,7,9,8]
 
-const filternum = numbers.filter(num=> num >7)
+const filternum = numbers.filter(num => num >7).map(num => num +5)
 
-const mapnum = filternum.map(num => num +5)
-
-console.log(mapnum)
+console.log(filternum)
