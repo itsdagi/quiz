19345db3318a6388038dca6ddb9b359cce2cefe4
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-11-22 09:06:39 +0000

    more chainging

--- chainingmethods.js
+++ chainingmethods.js
@@ -1,5 +1,5 @@
 const numbers =[1,5,6,4,3,2,7,9,8]
 
-const filternum = numbers.filter(num => num >7).map(num => num +5)
+const filternum = numbers.reverse().filter(num => num >7).map(num => num +5)
 
 console.log(filternum)
