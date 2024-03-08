7b2ccf5da5fd4fbb3a607b35eaa5094e9ce94d74
Dagim Alemu <133212131+dagimalemux@users.noreply.github.com>
2024-03-08 08:37:22 +0300

    Update chainingmethods.js

--- chainingmethods.js
+++ chainingmethods.js
@@ -1,8 +1,8 @@
 const numbers =[1,5,6,4,3,2,7,9,8]
 
-const filternum = numbers
-  .reverse()
-  .filter(num => num >7)
-  .map(num => num +5)
+const filternum = numbers;
+  .reverse();
+  .filter(num => num >7);
+  .map(num => num +5);
 
-console.log(filternum)
+console.log(filternum);
