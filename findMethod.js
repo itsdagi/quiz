d7fba80bc7cabaf6b0c97a93e6aa3debd13f0e1f
Dagim Alemu <133212131+dagimalemux@users.noreply.github.com>
2024-03-08 08:38:28 +0300

    Update findMethod.js

--- findMethod.js
+++ findMethod.js
@@ -3,6 +3,6 @@ const numbers = [5,20,146,40,130];
 const findNums = numbers.find((num)=>{
 
   return num > 50;
-})
+});
 
 console.log(findNums); // find the first element that satisfys a condition;
